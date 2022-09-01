a073aa9285cead96bb0c18c7b92e79a7c414cef1
Nikolai_Cholakov <nikolaicholakov25@gmail.com>
2022-09-01 10:38:54 +0300

    adding hpsting

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEPCa-m8bI_fcLbzwD7PAhllQl620vEFc",
+  authDomain: "the-cocktails-bar.firebaseapp.com",
+  projectId: "the-cocktails-bar",
+  storageBucket: "the-cocktails-bar.appspot.com",
+  messagingSenderId: "1007923227407",
+  appId: "1:1007923227407:web:ca3febed9d8c474853ec7a"
+};
+
+const app = initializeApp(firebaseConfig);
