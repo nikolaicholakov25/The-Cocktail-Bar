988799e3a57535abe604efebf2861653febd5847
Nikolai_Cholakov <nikolaicholakov25@gmail.com>
2022-09-09 17:44:43 +0300

    fixed webpack issue for the googleMap

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,12 @@ module.exports = {
         rules: [
             {
                 test: /\bmapbox-gl-csp-worker.js\b/i,
-                use: { loader: 'worker-loader' }
+                use: { 
+                    loader: 'babel-loader',
+                    options: {
+                        ignore: [ './node_modules/mapbox-gl/dist/mapbox-gl.js' ]
+                    },
+                        },
             }
         ]
     }
